125afbf0eafedbe97645cf64fa7fb82fbfc7cb96
Tanmay <tanmay01.ola@gmail.com>
2026-06-03 09:16:43 +0530

    useMemo cohort assignment

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+import { Assignment1 } from "./components/useMemoAssignment";
+import "./index.css"; 
+export function App() {
+  return (
+    <Assignment1/>
+  );
+}
+
+export default App;
---
+++ src/components/useMemoAssignment.tsx
@@ -0,0 +1,23 @@
+import { useMemo, useState } from "react";
+// In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
+// Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
+export function Assignment1() {
+    const [input, setInput] = useState(0);
+    const factorialCalculation = useMemo(()=>{
+      let result = 1; 
+      for (let i =1 ;i<=input ;i++){
+        result =  result*i;
+      }
+              return result
+
+    } ,[input])
+    return (
+        <div>
+            <input 
+                type="number"
+                onChange={(e)=>setInput(Number(e.target.value))}
+            />
+            <p>Calculated Value: {factorialCalculation}</p>
+        </div>
+    );
+}
